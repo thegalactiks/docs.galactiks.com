6a3544fb90921e29fcf7aacbf11eb465c8770c1d
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-02-15 16:29:19 +0100

    feat: fix remaining blog link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -112,10 +112,6 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/thegalactiks',
             },
